948ee000533c4acd5732539bc53a71db5475c217
Joel Timothy Oh <joeltimothyoh@live.com>
2019-09-21 02:32:29 +0800

    Add Get-ChildItem

---
+++ Get-ChildItem.ps1
@@ -0,0 +1,37 @@
+$args = @{
+    Path = @(
+        # 'S:'
+    )
+    # Filter = ''
+    # Include = ''
+    # Exclude = ''
+    # Recurse = $null
+    # Depth = 0
+    # Force = $null
+    # Name = $null
+    # Attributes = @(
+        # 'Archive'
+        # 'Compressed'
+        # 'Device'
+        # 'Directory'
+        # 'Encrypted'
+        # 'Hidden'
+        # 'IntegrityStream'
+        # 'Normal'
+        # 'NoScrubData'
+        # 'NotContentIndexed'
+        # 'Offline'
+        # 'ReadOnly'
+        # 'ReparsePoint'
+        # 'SparseFile'
+        # 'System'
+        # 'Temporary'
+    # )
+    # Directory = $null
+    # File = $null
+    # Hidden = $null
+    # ReadOnly = $null
+    # System = $null
+}
+
+Get-ChildItem @args
